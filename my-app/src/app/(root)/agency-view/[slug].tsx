8da0a67e116e3e1d0c30a8b5e8f038245593bdef
santoshdahal <dahalsantosh225@gmail.com>
2025-11-17 22:03:45 +0545

    second commit

---
+++ my-app/src/app/(root)/agency-view/[slug].tsx
@@ -0,0 +1,3 @@
+export default function AgencyView() {
+  return <div>[slug]</div>;
+}
---
+++ my-app/src/app/admin/page.tsx
@@ -0,0 +1,5 @@
+//this is login page for admin
+
+export default function page() {
+  return <div>page</div>;
+}
